c49d5d5de8a039f59c708da1518bbb52cbe72e79
Dmity <xtorer@ya.ru>
2022-09-15 11:21:40 +0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Dmitry on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
